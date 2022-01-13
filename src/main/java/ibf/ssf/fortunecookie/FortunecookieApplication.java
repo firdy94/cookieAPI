4a96bde683638026e1353ed2abb8ea10177727b8
Firdaus <mohdfirdaus94@outlook.com>
2022-01-13 13:49:42 +0800

    Initial commit

---
+++ src/main/java/ibf/ssf/fortunecookie/FortunecookieApplication.java
@@ -0,0 +1,13 @@
+package ibf.ssf.fortunecookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortunecookieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortunecookieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ibf/ssf/fortunecookie/FortunecookieApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf.ssf.fortunecookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortunecookieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
